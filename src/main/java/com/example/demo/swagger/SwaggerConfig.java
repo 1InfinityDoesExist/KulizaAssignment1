b8ebea9fa4ab57e793fb852dfc587c148cd1b263
1InfinityDoesExist <patelavinash508@gmail.com>
2021-05-14 13:13:19 +0545

    swagger config added

--- src/main/java/com/example/demo/swagger/SwaggerConfig.java
+++ src/main/java/com/example/demo/swagger/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.swagger;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,11 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -19,21 +19,20 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo")).paths(regex("/api/object/.*")).build()
+				.apis(RequestHandlerSelectors.basePackage("com.gaian.template")).paths(regex("/.*")).build()
 				.apiInfo(metaData());
 	}
 
 	private ApiInfo metaData() {
-		ApiInfo apiInfo = new ApiInfo("Kuliza Assignment", "Kuliza Rest Api For Assignment", "1.0", "Terms of service",
-				new Contact("Avinash Patel", "https://www.1InfinityDoesExist.com", "avinash.patel@kuliza.com"),
-				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+		@SuppressWarnings("deprecation")
+		ApiInfo apiInfo = new ApiInfo("Assignment RestCall Docs", "Kuliza Assignment", "1.0", "Terms of service",
+				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0", "Kuliza Technology");
 		return apiInfo;
 	}
 
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 
